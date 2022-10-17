31a05420e6576f720e0d4e1a1c7631961f37a749
test <test@test.com>
2022-10-17 23:50:41 +0900

    feat: klip api test

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,47 +4,74 @@ import logo from "../../assets/logoSymbolWithText.png";
 import Button from "../UI/Button/Button";
 import AuthContext from "../../store/auth-context";
 
-// const ethers = require("ethers");
+// klip api test
+import * as KlipAPI from "../../api/UseKlip";
+import QRCode from "qrcode.react";
+import { DEFAULT_ADDRESS } from "../../constants";
 
-const Login = () => {
+const Login = (props) => {
+  //--------------------- klip api test! ---------------------
+  const [qrvalue, setQrvalue] = useState(QRCode.default);
+
+  const [myAddress, setMyAddress] = useState(DEFAULT_ADDRESS);
+  const [showQR, setShowQR] = useState(false);
+
+  const getUserData = async () => {
+    KlipAPI.getAddress(setQrvalue, async (address) => {
+      authCtx.onLogin(address);
+      setMyAddress(address);
+    });
+    setShowQR(true);
+  };
+  //--------------------- klip api test! ---------------------
+
   const authCtx = useContext(AuthContext);
 
-  const ethers = require("ethers");
-  const [isConnected, setIsConnected] = useState(true);
+  // const ethers = require("ethers");
   // const [signer, setSigner] = useState();
-
+  // const [isConnected ,setIsConnected] = useState(false)
   const loginHandler = () => {
-    wallecConnect();
+    // wallecConnect();
+    getUserData();
   };
 
-  async function wallecConnect() {
-    if (typeof window.ethereum !== "undefined") {
-      try {
-        await window.ethereum.request({ method: "eth_requestAccounts" });
-        setIsConnected(true);
-        let connectedProvider = new ethers.providers.Web3Provider(
-          window.ethereum
-        );
-        // setSigner(connectedProvider.getSigner());
-        connectedProvider
-          .getSigner()
-          .getAddress()
-          .then((_address) => {
-            setIsConnected(authCtx.onLogin(_address));
-          });
-      } catch (e) {
-        console.log(e);
-      }
-    } else {
-      setIsConnected(false);
-    }
-  }
+  // async function wallecConnect() {
+  //   if (typeof window.ethereum !== "undefined") {
+  //     try {
+  //       await window.ethereum.request({ method: "eth_requestAccounts" });
+  //       setIsConnected(true);
+  //       let connectedProvider = new ethers.providers.Web3Provider(
+  //         window.ethereum
+  //       );
+  //       // setSigner(connectedProvider.getSigner());
+  //       connectedProvider
+  //         .getSigner()
+  //         .getAddress()
+  //         .then((_address) => {
+  //           setIsConnected(authCtx.onLogin(_address));
+  //         });
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //   } else {
+  //     setIsConnected(false);
+  //   }
+  // }
 
   return (
     <main>
       <img className={styles.logo} src={logo} alt="Blockchain Valley Logo" />
-      {isConnected ? null : <div> Chrome 브라우저에서 다시 시도해 주세요!</div>}
+      {/* {authCtx.isConnected ? null : <div> Chrome 브라우저에서 다시 시도해 주세요!</div>} */}
       <Button onClick={loginHandler}>Connect wallet</Button>
+      {showQR ? (
+        <QRCode
+          value={qrvalue}
+          size={100}
+          bgColor="#7FBC7F"
+          style={{ margin: "20px" }}
+        ></QRCode>
+      ) : null}{" "}
+      <div>{myAddress}</div>
     </main>
   );
 };
